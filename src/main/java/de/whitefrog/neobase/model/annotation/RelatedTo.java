8db78a89f242265d0ce7ca3ca2c24667ab577bbc
Jochen Weis <jochen@whitefrog.de>
2017-02-16 13:40:40 +0100

    added comments

--- src/main/java/de/whitefrog/neobase/model/annotation/RelatedTo.java
+++ src/main/java/de/whitefrog/neobase/model/annotation/RelatedTo.java
@@ -8,8 +8,24 @@ import java.lang.annotation.*;
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface RelatedTo {
+  /**
+   * The relationship type name to use.
+   */
   String type() default "None";
+
+  /**
+   * The relationships direction.
+   */
   Direction direction() default Direction.OUTGOING;
+
+  /**
+   * Allows multiple relationships with the same nodes.
+   * So Node A can have two relationships to Node B with the same name.
+   */
   boolean multiple() default false;
+
+  /**
+   * Restricts the type to the annotated one.
+   */
   boolean restrictType() default false;
 }
