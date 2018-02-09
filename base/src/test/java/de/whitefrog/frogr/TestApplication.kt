f487561232bbca0fc9a2b828e1e211d6a77f1c82
Jochen Weis <jochen@whitefrog.de>
2018-02-09 02:08:40 +0100

    added application registry test

--- base/src/test/java/de/whitefrog/frogr/TestApplication.kt
+++ base/src/test/java/de/whitefrog/frogr/TestApplication.kt
@@ -1,4 +1,16 @@
 package de.whitefrog.frogr
 
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+
 class TestApplication {
+  @Test
+  fun register() {
+    val app = de.whitefrog.frogr.test.TestApplication()
+    app.register("test")
+    assertThat(app.registry()).contains("test")
+    app.register("test2;test3")
+    assertThat(app.registry()).contains("test2")
+    assertThat(app.registry()).contains("test3")
+  }
 }
